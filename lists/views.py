d1e39cc76ea937ad592e876dadd373776971620c
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-07-02 03:16:33 -0400

    Comment explaining what is a response

--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, 'home.html')
+    response = render(request, 'home.html')
+    # response is an HttpResponse
+    # whose .content is the html from home.html
+    return response
