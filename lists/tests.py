615554e62a812573561c50676fcfc180dbcf915f
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-07-01 21:44:12 -0400

    Add unit test for home page

---
+++ lists/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+
+class HomePageTest(TestCase):
+
+    def test_home_page(self):
+        # Django has shortcuts instead of using selenium
+        response = self.client.get('/')
+        self.assertIn(
+            '<title>To-Do</title>',
+            response.content.decode()
+        )
+        self.assertTrue(response.content.decode().startswith('<html>'))
+        self.assertTrue(response.content.decode().endswith('</html>'))
