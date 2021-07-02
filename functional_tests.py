9c9ad56b58be2c76f28ca68197557d5c4b5eefe3
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-07-01 21:22:36 -0400

    Debug test and add comments for user story

--- functional_tests.py
+++ functional_tests.py
@@ -11,8 +11,18 @@ class HomePageTest(unittest.TestCase):
         self.browser.quit()
 
     def test_home_page(self):
+        # User visits site
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do' in self.browser.title)
+
+        # Title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-Do', header.text)
+
+        # User can input text into a text box
+        # User can press enter to update page with new to-do item
+        # There is still a text box to input more text
+        # User can enter another item
 
 
 if __name__ == '__main__':
